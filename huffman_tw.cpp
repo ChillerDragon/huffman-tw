cf63a0e969f0b41a95ea32aa3c8159ea0c0dba43
ChillerDragon <ChillerDragon@gmail.com>
2022-10-25 10:25:04 +0200

    Try to avoid bufferoverflows and raise instead

--- huffman_tw.cpp
+++ huffman_tw.cpp
@@ -5,22 +5,51 @@
 
 using namespace Rice;
 
+#define MAX_DATA_SIZE 2048
+
+int check_size(int Size)
+{
+    if (Size < MAX_DATA_SIZE)
+        return 1;
+
+    // TODO: fallback to memory allocation
+    //       instead of error
+    Object o;
+    String str("Data size is too big (maximum 2048)");
+    o.call("raise", str);
+    return 0;
+}
+
 Object huff_compress(const char * pInput, int Size)
 {
+    if (!check_size(Size))
+        return Qnil;
+
     CHuffman Huffman;
     Huffman.Init();
     unsigned char aOutput[2048];
     int CompressedSize = Huffman.Compress(pInput, Size, aOutput, sizeof(aOutput));
+
+    if (!check_size(CompressedSize))
+        return Qnil;
+
     Array out(aOutput, aOutput + CompressedSize);
     return out;
 }
 
 Object huff_decompress(const char * pInput, int Size)
 {
+    if (!check_size(Size))
+        return Qnil;
+
     CHuffman Huffman;
     Huffman.Init();
     unsigned char aOutput[2048];
     int DecompressedSize = Huffman.Decompress(pInput, Size, aOutput, sizeof(aOutput));
+
+    if (!check_size(DecompressedSize))
+        return Qnil;
+
     Array out(aOutput, aOutput + DecompressedSize);
     return out;
 }
